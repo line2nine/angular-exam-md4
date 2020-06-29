d6b9b3dd6cf5b1c5cc0c3633a0d31718d147264c
line2nine <hailinh203@gmail.com>
2020-06-29 10:17:58 +0700

    Create Project and Working On It

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './components/books/list/list.component';
+import { CreateComponent } from './components/books/create/create.component';
+import { EditComponent } from './components/books/edit/edit.component';
+import { DetailComponent } from './components/books/detail/detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'book/list', component: ListComponent
+  },
+  {
+    path: 'book/create-new', component: CreateComponent
+  },
+  {
+    path: 'book/:id/edit', component: EditComponent
+  },
+  {
+    path: 'book/:id/detail', component: DetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListComponent } from './components/books/list/list.component';
+import { CreateComponent } from './components/books/create/create.component';
+import { DetailComponent } from './components/books/detail/detail.component';
+import { EditComponent } from './components/books/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListComponent,
+    CreateComponent,
+    DetailComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
